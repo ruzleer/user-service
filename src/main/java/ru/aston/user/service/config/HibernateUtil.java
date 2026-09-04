87f17394627e98579c15e1fd04a1e41572dc22b1
azureeke <gygygygyllll24@gmail.com>
2026-09-04 21:22:39 +0200

    feat: add hibernate config

---
+++ src/main/java/ru/aston/user/service/config/HibernateUtil.java
@@ -0,0 +1,16 @@
+package ru.aston.user.service.config;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static final SessionFactory SESSION_FACTORY =
+            new Configuration()
+                    .configure()
+                    .buildSessionFactory();
+
+    public static SessionFactory getSessionFactory() {
+        return SESSION_FACTORY;
+    }
+}
