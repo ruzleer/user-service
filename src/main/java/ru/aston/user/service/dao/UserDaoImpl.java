5d946ca456162fcce84b838f68c104b96f9e7bb4
SmnVadim <115168791+SmnVadim@users.noreply.github.com>
2026-09-05 16:57:47 +0300

    created implementation UserDao interface

---
+++ src/main/java/ru/aston/user/service/dao/UserDaoImpl.java
@@ -0,0 +1,92 @@
+package ru.aston.user.service.dao;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import ru.aston.user.service.config.HibernateUtil;
+import ru.aston.user.service.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public User save(User user) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(user);
+            transaction.commit();
+            return user;
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Failed to save user", e);
+        }
+    }
+
+    @Override
+    public User update(User user) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            User updatedUser = session.merge(user);
+            transaction.commit();
+            return updatedUser;
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Failed to update user", e);
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            User user = session.get(User.class, id);
+            if (user != null) {
+                session.remove(user);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Failed to update user", e);
+        }
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            User user = session.get(User.class, id);
+            return Optional.ofNullable(user);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to find user", e);
+        }
+    }
+
+    @Override
+    public List<User> findAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<User> query = session.createQuery("FROM User ORDER BY id", User.class);
+            return query.list();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to find users", e);
+        }
+    }
+
+    @Override
+    public boolean existByEmail(String email) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            User user = session.get(User.class, email);
+            return user != null;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to check email existence", e);
+        }
+    }
+}
