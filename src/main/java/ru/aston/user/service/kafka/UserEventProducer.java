8e42690414eab8c88629476e7964442795b4dd09
azureeke <gygygygyllll24@gmail.com>
2026-09-25 15:07:52 +0200

    refactor: add constructor in UserEventProducer so lombock can see dynamic topic

--- src/main/java/ru/aston/user/service/kafka/UserEventProducer.java
+++ src/main/java/ru/aston/user/service/kafka/UserEventProducer.java
@@ -1,17 +1,18 @@
 package ru.aston.user.service.kafka;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class UserEventProducer {
     private final KafkaTemplate<String, UserEvent> kafkaTemplate;
-
-    @Value("${spring.kafka.topic.user-events:user-events}")
     private final String topic;
+
+    public UserEventProducer(KafkaTemplate<String, UserEvent> kafkaTemplate, @Value("${spring.kafka.topic.user-events:user-events}") String topic){
+        this.kafkaTemplate = kafkaTemplate;
+        this.topic = topic;
+    }
 
     public void send(UserEvent event) {
         kafkaTemplate.send(topic, event.email(), event);
